0f9ae298ed648c84c03e7b221d8334a51491330b
SoyJoaquinSoto <jsoto1800@alumno.ipn.mx>
2021-08-06 13:24:04 -0500

    Entrega de ejercicio 4.2

--- index.js
+++ index.js
@@ -1,22 +1,6 @@
-const config = require("./utils/config");
+const app = require("./app");
 const http = require("http");
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const blogsRouter = require("./controllers/blogs");
-const mongoose = require("mongoose");
-
-const mongoUrl = config.MONGODB_URI;
-mongoose.connect(mongoUrl, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-	useFindAndModify: false,
-	useCreateIndex: true,
-});
-
-app.use(cors());
-app.use(express.json());
-app.use("/api/blogs", blogsRouter);
+const config = require("./utils/config");
 
 const PORT = config.PORT;
 app.listen(PORT, () => {
