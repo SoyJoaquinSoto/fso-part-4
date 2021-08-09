9ba1fe5c3548a2652bb3d42b2ffbfdc60138e260
SoyJoaquinSoto <jsoto1800@alumno.ipn.mx>
2021-08-09 15:14:25 -0500

    Entrega de ejercicio 4.10

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -4,31 +4,57 @@ const app = require("../app");
 const helper = require("./test_helper.js");
 const Blog = require("../models/blog");
 
-beforeEach(async () => {
-	await Blog.deleteMany({});
+describe("blogs management", () => {
+	beforeEach(async () => {
+		await Blog.deleteMany({});
 
-	for (let blog of helper.initialBlogs) {
-		let blogObject = new Blog(blog);
-		await blogObject.save();
-	}
-});
-
-const api = supertest(app);
-
-test("the list of blogs has the correct size and format", async () => {
-	const response = await api.get("/api/blogs").expect("Content-Type", /json/);
-
-	expect(response.body).toHaveLength(helper.initialBlogs.length);
-});
-
-test("the unique identifier is defined as id", async () => {
-	const response = await api.get("/api/blogs");
-
-	response.body.forEach((blogPost) => {
-		expect(blogPost.id).toBeDefined();
+		for (let blog of helper.initialBlogs) {
+			let blogObject = new Blog(blog);
+			await blogObject.save();
+		}
 	});
-});
 
-afterAll(() => {
-	mongoose.connection.close();
+	const api = supertest(app);
+
+	test("the list of blogs has the correct size and format", async () => {
+		const response = await api
+			.get("/api/blogs")
+			.expect("Content-Type", /application\/json/);
+
+		expect(response.body).toHaveLength(helper.initialBlogs.length);
+	});
+
+	test("the unique identifier is defined as id", async () => {
+		const response = await api.get("/api/blogs");
+
+		response.body.forEach((blogPost) => {
+			expect(blogPost.id).toBeDefined();
+		});
+	});
+
+	test("a blog can be created correctly", async () => {
+		const newBlog = {
+			title: "New test blog",
+			author: "Joaquín Soto",
+			url: "https://coolurl.com/",
+			likes: 3,
+		};
+
+		await api
+			.post("/api/blogs")
+			.send(newBlog)
+			.expect(201)
+			.expect("Content-Type", /application\/json/);
+
+		const blogsAtEnd = await helper.blogsInDb();
+		expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+
+		const { id, ...lastBlog } = blogsAtEnd[helper.initialBlogs.length];
+
+		expect(lastBlog).toEqual(newBlog);
+	});
+
+	afterAll(() => {
+		mongoose.connection.close();
+	});
 });
