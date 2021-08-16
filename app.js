c7ad80d4990221163fb8fe5bec68baa1e8778829
SoyJoaquinSoto <jsoto1800@alumno.ipn.mx>
2021-08-16 08:29:20 -0500

    Agregué el middleware para loggear, de errores y de endpoint desconocido

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const blogsRouter = require("./controllers/blogs");
+const middleware = require("./utils/middleware");
 const mongoose = require("mongoose");
 
 const mongoUrl = config.MONGODB_URI;
@@ -15,6 +16,11 @@ mongoose.connect(mongoUrl, {
 
 app.use(cors());
 app.use(express.json());
+app.use(middleware.requestLogger);
+
 app.use("/api/blogs", blogsRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 module.exports = app;
